999c92eedaa82c48f4e578b86273e1b3f2e3285c
Wonchang0314 <andrew0314@naver.com>
2025-06-24 04:50:30 +0900

    FIX: github pages를 위한 스토리북 설정 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,22 +1,4 @@
 import '../src/app/globals.css';
-import * as NextImage from 'next/image';
-
-// Next.js Image 컴포넌트를 일반 img 태그로 대체
-const OriginalNextImage = NextImage.default;
-
-Object.defineProperty(NextImage, 'default', {
-  configurable: true,
-  value: props => {
-    if (typeof props.src === 'string') {
-      // GitHub Pages 배포시 base path 추가
-      const basePath = process.env.NODE_ENV === 'production' ? '/DDD-12-GROWIT-FE' : '';
-      const src = props.src.startsWith('/') ? `${basePath}${props.src}` : props.src;
-
-      return <img {...props} src={src} alt={props.alt || ''} />;
-    }
-    return <OriginalNextImage {...props} />;
-  },
-});
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
