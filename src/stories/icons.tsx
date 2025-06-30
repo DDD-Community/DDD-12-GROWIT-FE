179f41569fc238741ec11741c34deeef88de4714
Wonchang0314 <andrew0314@naver.com>
2025-07-01 00:10:18 +0900

    FEAT: 버튼 상태 추가 및 스타일 업로드 유틸 함수 개발

---
+++ src/stories/icons.tsx
@@ -0,0 +1,46 @@
+type IconKey = 'circle' | 'whiteCircle';
+
+const circle = (
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g clip-path="url(#clip0_429_3017)">
+      <path
+        d="M10 18.3334C14.6024 18.3334 18.3333 14.6025 18.3333 10.0001C18.3333 5.39771 14.6024 1.66675 10 1.66675C5.39763 1.66675 1.66667 5.39771 1.66667 10.0001C1.66667 14.6025 5.39763 18.3334 10 18.3334Z"
+        stroke="#171719"
+        stroke-width="1.67"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_429_3017">
+        <rect width="20" height="20" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+const whiteCircle = (
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g clip-path="url(#clip0_429_3017)">
+      <path
+        d="M10 18.3334C14.6024 18.3334 18.3333 14.6025 18.3333 10.0001C18.3333 5.39771 14.6024 1.66675 10 1.66675C5.39763 1.66675 1.66667 5.39771 1.66667 10.0001C1.66667 14.6025 5.39763 18.3334 10 18.3334Z"
+        stroke="white"
+        stroke-width="1.67"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_429_3017">
+        <rect width="20" height="20" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+const Icons: Record<IconKey, React.ReactNode> = {
+  circle: circle,
+  whiteCircle: whiteCircle,
+};
+
+export default Icons;
