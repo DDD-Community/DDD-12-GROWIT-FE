8ca70bd2c2446938631186fb2e92fa6fc290bca9
icebear9028 <tlatms9028@gmail.com>
2025-07-29 22:51:58 +0900

    fix: Input 및 TextArea 컴포넌트 focus 색상 변경

--- src/shared/components/input/InputField.tsx
+++ src/shared/components/input/InputField.tsx
@@ -17,8 +17,8 @@ export function InputField({ label, isError, errorMessage, description, ...props
         <input
           maxLength={props.maxLength}
           {...props}
-          className={`w-full h-full px-4 py-3 rounded-lg bg-[#2C2C2E] text-white placeholder-gray-500 border-none focus:ring-2 ${
-            isError ? 'focus:ring-red-500 ring-2 ring-red-500' : 'focus:ring-[#8C7FF7]'
+          className={`w-full h-full px-4 py-3 rounded-lg bg-[#2C2C2E] text-white border border-label-assistive placeholder-gray-500 focus:ring-2 ${
+            isError ? 'focus:ring-red-500 ring-2 ring-red-500' : 'focus:ring-primary-normal'
           }`}
         />
         {isError && (
