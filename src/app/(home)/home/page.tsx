78a8bf415185a8e08cbf9630d73e136db103506a
icebear9028 <tlatms9028@gmail.com>
2025-08-08 16:07:59 +0900

    feat: cheerMessage 위치 변경에 따른 레이아웃 개선

--- src/app/(home)/home/page.tsx
+++ src/app/(home)/home/page.tsx
@@ -8,21 +8,21 @@ import { GoalProvider } from '@/shared/hooks';
 export default function MainPage() {
   return (
     <GoalProvider>
-      <div className="flex w-full max-sm:flex-col">
-        {/* 메인 레이아웃 */}
-        <div className="flex flex-col sm:overflow-y-scroll sm:flex-1 pt-[32px] pb-[48px]">
-          <div className="flex flex-col max-sm:mx-[20px] sm:mx-[40px] gap-[48px]">
-            <Image src="/logo-text.svg" alt="logo" width={86} height={32} />
-            <CheerMessageCard />
+      <div className="flex w-full flex-col">
+        <div className="max-sm:mx-[20px] sm:mx-[40px] mt-[32px] mb-[20px]">
+          <CheerMessageCard />
+        </div>
+        <div className="flex max-sm:flex-col overflow-y-scroll sm:flex-1 mt-[32px] mb-[48px]">
+          {/* 메인 레이아웃 */}
+          <div className="flex flex-col flex-1 max-sm:mx-[20px] sm:mx-[40px] gap-[48px]">
             <PlanBoard />
           </div>
-        </div>
-        {/* 서브 레이아웃 - 로드맵 & 잔디그래프 확인 */}
-        <div className="flex flex-col gap-8 sm:overflow-y-scroll p-[16px] md:border-l border-line-normal w-[335px] mx-auto">
-          {/* Todo 추가 버튼 */}
-          {/* <GoalRoadMap.Desktop />
-          <ContributionGraph />
-          <GoalRoadMap.Mobile /> */}
+          {/* 서브 레이아웃 - 로드맵 & 잔디그래프 확인 */}
+          <div className="flex flex-col gap-8 sm:overflow-y-scroll md:border-l border-line-normal w-[335px] max-sm:mx-auto">
+            {/* <GoalRoadMap.Desktop />
+            <ContributionGraph />
+            <GoalRoadMap.Mobile /> */}
+          </div>
         </div>
       </div>
     </GoalProvider>
