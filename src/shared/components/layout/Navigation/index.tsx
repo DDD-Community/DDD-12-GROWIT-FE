7e2b9b4a0f55344bc17977deabbc484d1d9560d9
icebear9028 <tlatms9028@gmail.com>
2025-08-24 14:19:06 +0900

    fea: Navigation 표시 여부 config 파일 추가

--- src/shared/components/layout/Navigation/index.tsx
+++ src/shared/components/layout/Navigation/index.tsx
@@ -1,7 +1,13 @@
+import { usePathname } from 'next/navigation';
 import { BottomNavigation } from './BottomNavigation';
 import { SideNaviagation } from './SideNavigation';
+import { shouldHiddenNavigation } from '@/shared/constants/routes';
 
 const NavigationBar = () => {
+  const pathname = usePathname();
+  if (shouldHiddenNavigation(pathname)) {
+    return null;
+  }
   return (
     <>
       <SideNaviagation />
