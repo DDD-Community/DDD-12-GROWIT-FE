f8881b2c27c7f4521e0cf18c1ecaad37294456af
icebear9028 <tlatms9028@gmail.com>
2025-10-06 20:22:17 +0900

    hotfix: 페이지별 레이아웃 적용

--- src/app/(home)/home/create-goal/layout.tsx
+++
@@ -1,7 +0,0 @@
-interface HomeLayoutProps {
-  children?: React.ReactNode;
-}
-
-export default function CreateGoalLayout({ children }: HomeLayoutProps) {
-  return <div className="flex flex-1 max-w-md">{children}</div>;
-}
