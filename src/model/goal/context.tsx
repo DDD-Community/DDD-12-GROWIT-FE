6d4d6e6ecb7435dc71fe3675e7da1d5f1d8c70bc
Wonchang0314 <andrew0314@naver.com>
2026-01-07 22:12:28 +0900

    fix: GoalProvider 안전하게 useQuery로 변경

--- src/model/goal/context.tsx
+++ src/model/goal/context.tsx
@@ -5,7 +5,7 @@ import { useState, createContext, useContext, createElement, useEffect } from 'r
 import { Goal } from '@/shared/type/goal';
 //import { useToast } from '@/shared/components/feedBack/toast';
 import { GoalQuery } from './queries';
-import { useSuspenseQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 
 // interface ServerRequest<Request> {
 //   request: Request;
@@ -15,6 +15,7 @@ import { useSuspenseQuery } from '@tanstack/react-query';
 
 interface GoalContextType {
   progressGoals: Goal[];
+  isLoadingGoals: boolean;
   currentGoal: Goal | null;
   setCurrentGoal: (goal: Goal | null) => void;
 }
@@ -28,27 +29,25 @@ export function GoalProvider({ children }: { children: React.ReactNode }) {
   //const { showToast } = useToast();
   const [currentGoal, setCurrentGoal] = useState<Goal | null>(null);
 
-  // 서버에서 prerendering 시점에 데이터를 불러올 수 없기 때문에 클라이언트 사이드에서만 쿼리 실행
-  const progressGoalQuery =
-    typeof window === 'undefined'
-      ? null
-      : useSuspenseQuery(
-          GoalQuery.getProgressGoals({
-            // 캐시 무효화가 없다면, 현재 시간부터 하루가 끝나기전까지 유지되어도 괜찮다 판단
-            staleTime: msUntilEndOfDay,
-            gcTime: msUntilEndOfDay,
-          })
-        );
+  // 서버에서 prerendering 시점에 데이터를 불러올 수 없기 때문에 useSuspenseQuery 대신에 클라이언트 사이드에서만 실행되는 useQuery로 대체
+  const { data: progressGoals = [], isLoading } = useQuery(
+    GoalQuery.getProgressGoals({
+      // 캐시 무효화가 없다면, 현재 시간부터 하루가 끝나기전까지 유지되어도 괜찮다 판단
+      staleTime: msUntilEndOfDay,
+      gcTime: msUntilEndOfDay,
+    })
+  );
 
   useEffect(() => {
     // 최초에 현재 선택된 목표값 초기화
-    if (progressGoalQuery && progressGoalQuery.data) {
-      setCurrentGoal(progressGoalQuery.data[0] || null);
+    if (progressGoals) {
+      setCurrentGoal(progressGoals[0] || null);
     }
-  }, [progressGoalQuery]);
+  }, [progressGoals]);
 
   const value: GoalContextType = {
-    progressGoals: progressGoalQuery ? progressGoalQuery.data : [],
+    progressGoals: progressGoals ? progressGoals : [],
+    isLoadingGoals: isLoading,
     currentGoal,
     setCurrentGoal,
   };
